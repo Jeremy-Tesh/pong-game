dce33297cb7280fd6f9feb878b5e1a52eaa98171
Jeremy Tesh <jermitesh20@gmail.com>
2022-04-12 11:36:18 +0300

    feat:game window

---
+++ Pong1.py
@@ -0,0 +1,12 @@
+import turtle
+import tkinter as TK
+
+wn =turtle.Screen()
+wn.title("pong by ermi")
+wn.bgcolor("black")
+wn.setup(width=800,height=600)
+wn.tracer(0)
+
+
+while True:
+    wn.update()
