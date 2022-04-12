56327af1d8d4517ad336bd536f874db40344e7eb
Jeremy Tesh <jermitesh20@gmail.com>
2022-04-12 13:10:21 +0300

    feat:move the ball with border check

--- Pong1.py
+++ Pong1.py
@@ -37,9 +37,10 @@ ball = turtle.Turtle()
 ball.speed(0)
 ball.shape("circle")
 ball.color("white")
-
 ball.penup()
 ball.goto(0,0)
+ball.dx = 0.1
+ball.dy = -0.1
 
 
 #Function
@@ -76,3 +77,25 @@ wn.onkeypress(paddle_b_down,'Down')
 
 while True:
     wn.update()
+
+    #Move the ball
+    ball.setx(ball.xcor() + ball.dx)
+    ball.sety(ball.ycor() + ball.dy)
+
+    #Border checking
+    if ball.ycor() > 290:
+        ball.sety(290)
+        ball.dy *= -1
+
+    if ball.ycor() < -290:
+        ball.sety(-290)
+        ball.dy *= -1    
+
+
+    if ball.xcor() > 390:
+        ball.setx(390)
+        ball.dx *= -1
+
+    if ball.xcor() < -390:
+        ball.setx(-390)
+        ball.dx *= -1        
